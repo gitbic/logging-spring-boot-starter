b71ad93ede54469251df7d92e669a7c20c42a323
pturskiy@clevertec.ru <pturskiy@clevertec.ru>
2021-06-22 13:24:53 +0300

    rewrite log messages and ad logging for exception

--- src/main/java/ru/clevertec/starter/aspect/LogMethodExecutionAspect.java
+++ src/main/java/ru/clevertec/starter/aspect/LogMethodExecutionAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import ru.clevertec.starter.constants.Description;
+import ru.clevertec.starter.constants.LoggingMessage;
 
 import java.util.Arrays;
 
@@ -21,29 +22,34 @@ public class LogMethodExecutionAspect {
 
     @Before("getDefaultPointcut()")
     public void logMethodBeforeExecution(JoinPoint joinPoint) {
-        log.warn(Description.BEFORE_METHOD_EXECUTING +
-                Description.METHOD_SIGNATURE + joinPoint.getSignature().toString() +
-                Description.METHOD_ARGUMENTS + Arrays.toString(joinPoint.getArgs()));
+
+        log.warn(LoggingMessage.BEFORE_METHOD_EXECUTION_MESSAGE,
+                joinPoint.getSignature().toString(), Arrays.toString(joinPoint.getArgs()));
     }
 
     @AfterReturning(pointcut = "getDefaultPointcut()", returning = "returningValues")
     public void logMethodAfterExecution(JoinPoint joinPoint, Object returningValues) {
-        log.warn(Description.AFTER_METHOD_EXECUTING +
-                Description.METHOD_SIGNATURE + joinPoint.getSignature().toString() +
-                Description.METHOD_ARGUMENTS + Arrays.toString(joinPoint.getArgs()) +
-                Description.METHOD_RETURNING_VALUES + returningValues);
+
+        log.warn(LoggingMessage.AFTER_METHOD_EXECUTION_MESSAGE,
+                joinPoint.getSignature().toString(), Arrays.toString(joinPoint.getArgs()), returningValues);
     }
 
     @Around("getDefaultPointcut()")
     public Object logMethodDuringExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+
         final long startTime = System.currentTimeMillis();
         final Object proceed = joinPoint.proceed();
-        final long executionTime = System.currentTimeMillis() - startTime;
+        final long methodExecutionTime = System.currentTimeMillis() - startTime;
 
-        log.warn(Description.DURING_METHOD_EXECUTING +
-                Description.METHOD_SIGNATURE + joinPoint.getSignature().toString() +
-                Description.METHOD_EXECUTING_TIME + executionTime);
-
+        log.warn(LoggingMessage.DURING_METHOD_EXECUTION_MESSAGE,
+                joinPoint.getSignature().toString(), methodExecutionTime);
         return proceed;
+    }
+
+    @AfterThrowing(pointcut = "getDefaultPointcut()", throwing = "exception")
+    public void logMethodAfterThrowingException(JoinPoint joinPoint, Exception exception) {
+
+        log.error(LoggingMessage.AFTER_METHOD_THROWING_EXCEPTION_MESSAGE,
+                joinPoint.getSignature().toString(), exception.getMessage());
     }
 }
--- src/main/java/ru/clevertec/starter/constants/Description.java
+++ src/main/java/ru/clevertec/starter/constants/Description.java
@@ -5,9 +5,15 @@ public class Description {
     public static final String DURING_METHOD_EXECUTING = "During method execution";
     public static final String BEFORE_METHOD_EXECUTING = "Before method executing";
     public static final String AFTER_METHOD_EXECUTING = "After method executing";
+    public static final String AFTER_THROWING_EXCEPTION = "After throwing exception";
     public static final String METHOD_ARGUMENTS = ". Method arguments: ";
     public static final String METHOD_SIGNATURE = ". Method signature: ";
     public static final String METHOD_RETURNING_VALUES = ". Method returning values: ";
     public static final String METHOD_EXECUTING_TIME = ". Method executing time: ";
+    public static final String EXCEPTION_MESSAGE = ". Exception message: ";
+
+
+
+
 
 }
